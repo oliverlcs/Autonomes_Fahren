aeb902069815695c5a83633a9dc8b1e41fadeded
Paul Schindhelm <paul.schindhelm@porsche-engineering.de>
2025-04-16 10:38:29 +0200

    kannten werden erkannt

--- project/lane_detection.py
+++ project/lane_detection.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.ndimage import uniform_filter
 
 class LaneDetection:
     def __init__(self):
@@ -12,18 +13,21 @@ class LaneDetection:
         threshold = 120  # Schwellenwert für die Trennung
         binary_mask = (grayscale > threshold).astype(np.uint8) * 255
 
+        # Glättung der binären Maske
+        smoothed_mask = uniform_filter(binary_mask, size=5)
+
         # X- und Y-Gradienten berechnen
-        grad_x = np.abs(np.diff(binary_mask, axis=1))  # horizontal
+        grad_x = np.abs(np.diff(smoothed_mask, axis=1))  # horizontal
         grad_x = np.pad(grad_x, ((0, 0), (1, 0)), mode='constant')
 
-        grad_y = np.abs(np.diff(binary_mask, axis=0))  # vertikal
+        grad_y = np.abs(np.diff(smoothed_mask, axis=0))  # vertikal
         grad_y = np.pad(grad_y, ((1, 0), (0, 0)), mode='constant')
 
         # Kantenstärke kombinieren
         edge_strength = grad_x + grad_y
 
         h, w = edge_strength.shape
-        edge_threshold = 80  # kann angepasst werden
+        edge_threshold = 50  # Reduzierter Schwellenwert für Kantenstärke
 
         left_points = []
         right_points = []
@@ -32,10 +36,19 @@ class LaneDetection:
             row = edge_strength[y]
 
             # Linker Rand: Suche von links
+            found_left = False
             for x in range(w // 2):
                 if row[x] > edge_threshold:
                     left_points.append((x, y))
+                    found_left = True
                     break
+
+            # Falls kein Punkt gefunden wurde, suche weiter rechts
+            if not found_left:
+                for x in range(w // 2, w // 2 + 20):  # Erweiterter Bereich
+                    if row[x] > edge_threshold:
+                        left_points.append((x, y))
+                        break
 
             # Rechter Rand: Suche von rechts
             for x in reversed(range(w // 2, w)):
